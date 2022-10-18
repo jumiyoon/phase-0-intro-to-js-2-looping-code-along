d090c39e3749a09f4c751d4a74f6b07c02a855b6
Jumi Yoon <jums.code@gmail.com>
2022-10-17 23:39:44 -0700

    commit tree 49b9cb23159e8475c5800331fb895948237d1e41

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+
+function writeCards(group) {
+    let greetings = []
+    for (let i = 0; i < group.length; i ++) {
+        greetings.push(`Thank you, ${group[i]}, for the wonderful surprise gift!`)
+    }
+    return greetings
+}
+
+function countDown(number) {
+    for (let i = 0; i < number+1 ; number --) {
+        console.log (number)
+    }
+}
